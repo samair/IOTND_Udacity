1aa0b41672c13d6574bba76f7afa0e8947242346
tatirs1 <sameer.tatiraju@emc.com>
2018-05-09 18:32:07 +0530

    Explored String functions

--- Strngs.py
+++ Strngs.py
@@ -1,5 +1,7 @@
 test_string = "this is a string"
 
-print(test_string.capitalize()) ##This is a string
-print(test_string.casefold())   ##this is a string
-print(test_string.center(25, "g")) ##gggggthis is a stringgggg
+print(test_string.capitalize()) # This is a string
+print(test_string.casefold())   # this is a string
+print(test_string.center(25, "g")) # gggggthis is a stringgggg
+print(test_string.find("a"))  # 8
+print(test_string.index("str"))  # 10
