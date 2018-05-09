1d756f2ba22facc7091a9f4ade14c66c42e55c49
Sameer Chandra <tsameerc@gmail.com>
2018-05-09 18:22:01 +0530

    Add files via upload

---
+++ Strngs.py
@@ -0,0 +1,5 @@
+test_string = "this is a string"
+
+print(test_string.capitalize()) ##This is a string
+print(test_string.casefold())   ##this is a string
+print(test_string.center(25, "g")) ##gggggthis is a stringgggg
