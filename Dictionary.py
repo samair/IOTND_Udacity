524c08b28afd8ced808c7d5d9d5b09a779250069
tatirs1 <sameer.tatiraju@emc.com>
2018-05-10 10:45:47 +0530

    Added new data types

---
+++ Dictionary.py
@@ -0,0 +1,6 @@
+elements = {"india": "+91", "US": "+1"}
+print(elements.get("india"))  # +91
+print(elements.get("pakistan"))  # None
+print("US" in elements)  # True
+
+
---
+++ test_list_Sets.py
@@ -0,0 +1,7 @@
+test_list = ["hello", "World"]
+test_list.append("Python")
+print(test_list) # ['hello', 'World', 'Python']
+
+list_1 = ["One ", "Two", "Three"]
+joined = "&".join(list_1)
+print(joined)
